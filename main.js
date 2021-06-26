f7c0023f4484af0ddd7f7485f522b6eacfc3e200
sumit <sumitprajapati821@gmail.com>
2021-06-26 16:39:44 +0530

    Added email form as index.html along with success page. Also using express package for routing

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-const http = require("http");
 const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+const nodemailer = require('nodemailer');
+const path = require('path');
 const { PassThrough } = require("stream");
 const { Console } = require("console");
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/login/compose") {
-        if (req.methode === "GET") {
-            fs.readFile("./templates/form.html", function (error, pgResp) {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(pgResp);
-                res.end();
-            });
-        }
-        else if (req.methode === "POST") {
-            let body = '';
-            req.on('data', chunk => {
-                body += chunk.toString(); // convert Buffer to string
-            });
-            req.on('end', () => {
-                console.log(body);
-                res.end('ok');
-            });
-        }
-    }
+
+const port = process.env.PORT || 3000;  //PORT
+
+const app = express();
+
+// View Engine Setup
+app.engine('handlebars',exphbs());
+app.set('view engine','handlebars');
+
+// Static folder
+app.use('/public',express.static(path.join(__dirname,'public')));
+
+// Body-parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+// EndPoints
+app.get('/',(req,res)=>{
+    fs.readFile("./templates/index.html", function (error, pgResp) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(pgResp);
+        res.end();
+    });
+}); 
+
+app.post('/send',(req,res)=>{
+    console.log(req.body);
+    fs.readFile("./templates/success.html", function (error, pgResp) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(pgResp);
+        res.end();
+    });
 });
+ 
 
-server.on('connection', (socket) => {
-    console.log("seeing");
+
+//START THE SERVER
+app.listen(port, () => {
+    console.log(`Listening on port:${port}`);
 })
-
-server.listen(3000);
-
-console.log("hello world");
